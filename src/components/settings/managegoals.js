d133e2b86eaabea062b5a5f59ec4a4863ad0654b
ikurakoosh <67201143+ikurakoosh@users.noreply.github.com>
2021-03-03 17:38:27 -0600

    delete habit issues

--- src/components/settings/managegoals.js
+++ src/components/settings/managegoals.js
@@ -1,38 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 //import './App.css';
 import {
     Link,
   } from 'react-router-dom'
   import {store} from '../../store.js';
 function ManageGoals() {
-  const MyVariables = useContext(store);
-  const {dispatch} = MyVariables
-    let goals = MyVariables.state.habits.map((goal, index) =>
-      <div key = {index}>
-        <input type="text" defaultValue= {goal.name} data-index = {index} onChange={callOnChange}/><button type="button" onClick={callDelete}> delete</button>
-      </div>
-    );
+  const MyStore = useContext(store);
+  const [goals, setGoals] = useState([...MyStore.state.habits])
+  const {dispatch} = MyStore
+  
     function callOnChange(event) {
-     alert(event.target.dataset.index)
      const goalIndex = event.target.dataset.index
      const goalValue = event.target.value
      dispatch({type: 'updateHabit', HabitIndex: goalIndex, HabitValue: goalValue})
     }
-  function callDelete(event) {
-    console.log("holii ")
-    const goalIndex = event.target.dataset.index
-    dispatch({type: 'deleteHabit', HabitIndex: goalIndex});
-    goals = <div>hola</div>
+  function callDelete(index) {
+    alert(index)
+    dispatch({type: 'deleteHabit', HabitIndex: index});
+    setGoals([...MyStore.state.habits])
     
   }
   function callAddNew() {
     alert('Add new. ');
   }
   return (
-
     <div className="manageGoals">
         input ur goals
-        {goals}
+        {
+          goals.map((goal, index) => (
+            <div><input type="text" defaultValue= {goal.name} data-index = {index} onChange={callOnChange}/><button type="button" onClick={()=>callDelete(index)}> delete</button></div>
+          ))
+        }
         <button type="button" onClick={callAddNew}> add new</button>
         <div>
         <Link to= "/Settings">back</Link>
